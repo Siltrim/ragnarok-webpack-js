98134820980fdf91a0c2e2af6046c434f9b5593b
Danil Vorontsov <rojerr@MacBook-Air-Danil.local>
2023-09-16 22:18:19 +0600

    added js logic modal window

--- src/assets/js/languages.js
+++ src/assets/js/languages.js
@@ -1,18 +1,26 @@
 export const languages = {
   en: {
-    editions: "Editions",
-    controller: "Controller",
-    about: "About",
-    explore: "Explore",
-    news: "News",
-    faq: "FAQ",
+    editions: 'Editions',
+    controller: 'Controller',
+    about: 'About',
+    explore: 'Explore',
+    news: 'News',
+    faq: 'FAQ',
+    description:
+      'Taking place three years following the events of the previous game, Fimbulwinter, a great winter that spans three summers, is drawing to a close which will begin the prophesied Ragnarök.		',
+    button: 'Buy now',
+    release: 'release',
   },
   ru: {
-    editions: "Издания",
-    controller: "Джойстик",
-    about: "Об игре",
-    explore: "Исследовать",
-    news: "Новости",
-    faq: "Помощь",
+    editions: 'Издания',
+    controller: 'Джойстик',
+    about: 'Об игре',
+    explore: 'Исследовать',
+    news: 'Новости',
+    faq: 'Помощь',
+    description:
+      'Действие происходит через три года после событий предыдущей игры, великая зима, продолжающаяся три лета, приближается к концу, после чего начнется Рагнарек',
+    button: 'Купить сейчас',
+    release: 'Релиз',
   },
 };
