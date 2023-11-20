1e0cc273150eedfd7825a1404e73fe5033f9b154
Siltrim <rojerr410@gmail.com>
2023-11-20 19:14:38 +0600

    public dir for vercel

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     clean: true,
     filename: '[name].bundle.js',
     assetModuleFilename: 'assets/images/[name][ext]',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
   mode: 'development',
   devServer: {
